1c95060482aec8c17160e6b89993f9ece7f7508e
axelmagnus <33330896+axelmagnus@users.noreply.github.com>
2023-01-01 15:15:20 +0100

    getting historic feed, w pyp, sparkline

--- code.py
+++ code.py
@@ -1,139 +1,99 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-# class of sparklines in CircuitPython
-# created by Kevin Matocha - Copyright 2020 (C)
-
-# See the bottom for a code example using the `sparkline` Class.
-
-# # File: display_shapes_sparkline.py
-# A sparkline is a scrolling line graph, where any values added to sparkline using
-# `add_value` are plotted.
-#
-# The `sparkline` class creates an element suitable for adding to the display using
-# `display.show(mySparkline)`
-# or adding to a `displayio.Group` to be displayed.
-#
-# When creating the sparkline, identify the number of `max_items` that will be
-# included in the graph.
-# When additional elements are added to the sparkline and the number of items has
-# exceeded max_items, any excess values are removed from the left of the graph,
-# and new values are added to the right.
-
-
-# The following is an example that shows the
-
-# setup display
-# instance sparklines
-# add to the display
-# Loop the following steps:
-# 	add new values to sparkline `add_value`
-# 	update the sparklines `update`
-
 import time
-import random
 import board
-import displayio
-
-
+from adafruit_pyportal import PyPortal
+from adafruit_esp32spi import adafruit_esp32spi
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+import adafruit_requests as requests
+from digitalio import DigitalInOut
+import busio
+import json
 from adafruit_display_shapes.sparkline import Sparkline
+import displayio
+from secrets import secrets
+import gc
 
-if "DISPLAY" not in dir(board):
-    # Setup the LCD display with driver
-    # You may need to change this to match the display driver for the chipset
-    # used on your display
-    from adafruit_ili9341 import ILI9341
+# Adafruit IO Account
+IO_USER = secrets['aio_username']
+IO_KEY = secrets['aio_key']
+# Adafruit IO Feed
+IO_FEED = 'ab-weather.winddirection'
 
-    displayio.release_displays()
+HOURS = 24
+maxdatapoints = 64
 
-    # setup the SPI bus
-    spi = board.SPI()
-    tft_cs = board.D9  # arbitrary, pin not used
-    tft_dc = board.D10
-    tft_backlight = board.D12
-    tft_reset = board.D11
+DATA_SOURCE = "https://io.adafruit.com/api/v2/{0}/feeds/{1}/data/chart?X-AIO-Key={2}".format(IO_USER,
+                                                                                  IO_FEED, IO_KEY)
 
-    while not spi.try_lock():
-        spi.configure(baudrate=32000000)
+DATA_SOURCE += "&hours=4"
 
-    spi.unlock()
+print(DATA_SOURCE)
 
-    display_bus = displayio.FourWire(
-        spi,
-        command=tft_dc,
-        chip_select=tft_cs,
-        reset=tft_reset,
-        baudrate=32000000,
-        polarity=1,
-        phase=1,
-    )
+""" FEED_VALUE_LOCATION = ['updated_at']
+#FEED_VALUE_LOCATION = ['last_value']
+# --------------- Adafruit io --------------- #
+# If you are using a board with pre-defined ESP32 Pins:
+esp32_cs = DigitalInOut(board.ESP_CS)
+esp32_ready = DigitalInOut(board.ESP_BUSY)
+esp32_reset = DigitalInOut(board.ESP_RESET)
 
-    print("spi.frequency: {}".format(spi.frequency))
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
 
-    # Number of pixels in the display
-    DISPLAY_WIDTH = 320
-    DISPLAY_HEIGHT = 240
+print("Connecting to AP...")
+while not esp.is_connected:
+    try:
+        esp.connect_AP(secrets["ssid"], secrets["password"])
+    except RuntimeError as e:
+        print("could not connect to AP, retrying: ", e)
+        continue
+print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
 
-    # create the display
-    display = ILI9341(
-        display_bus,
-        width=DISPLAY_WIDTH,
-        height=DISPLAY_HEIGHT,
-        # The rotation can be adjusted to match your configuration.
-        rotation=180,
-        auto_refresh=True,
-        native_frames_per_second=90,
-    )
+socket.set_interface(esp)
+requests.set_socket(socket, esp) """
 
-    # reset the display to show nothing.
-    display.show(None)
-else:
-    # built-in display
-    display = board.DISPLAY
 
-##########################################
-# Create background bitmaps and sparklines
-##########################################
+cwd = ("/"+__file__).rsplit('/', 1)[0]
+pyportal = PyPortal(url=DATA_SOURCE,
+                   
+                    status_neopixel=board.NEOPIXEL,
+                    #default_bg=cwd+"/pyportal_email.bmp",
+                    text_font=cwd+"/fonts/Helvetica-Oblique-17.bdf",
+                    text_position=(30, 65),
+                    text_color=0xFFFFFF,
+                    text_wrap=35,  # wrap feed after 35 chars
+                    text_maxlen=160)
 
-# Baseline size of the sparkline chart, in pixels.
-chart_width = display.width
-chart_height = display.height
-
-# sparkline1 uses a vertical y range between 0 to 10 and will contain a
-# maximum of 40 items
+# speed up projects with lots of text by preloading the font!
+pyportal.preload_font()
 sparkline1 = Sparkline(
-    width=chart_width, height=chart_height, max_items=40, y_min=0, y_max=10, x=0, y=0
+    width=400, height=200, max_items=100, x=20, y=50
 )
-
-# Create a group to hold the sparkline and append the sparkline into the
-# group (my_group)
-#
-# Note: In cases where display elements will overlap, then the order the elements
-# are added to the group will set which is on top.  Latter elements are displayed
-# on top of former elements.
 my_group = displayio.Group()
+display = board.DISPLAY
 
 # add the sparkline into my_group
 my_group.append(sparkline1)
 
-
 # Add my_group (containing the sparkline) to the display
 display.show(my_group)
 
-# Start the main loop
 while True:
-
-    # turn off the auto_refresh of the display while modifying the sparkline
     display.auto_refresh = False
-
-    # add_value: add a new value to a sparkline
-    # Note: The y-range for mySparkline1 is set to 0 to 10, so all these random
-    # values (between 0 and 10) will fit within the visible range of this sparkline
-    sparkline1.add_value(random.uniform(0, 10))
-
-    # turn the display auto_refresh back on
+    try:
+        print('Fetching Adafruit IO Feed Value..')
+        value = pyportal.fetch() 
+        print("Response is", value)
+        print("____________________________________________")
+        #value = requests.get(DATA_SOURCE)
+        jsondata = json.loads(value)['data']
+        print(jsondata)
+        for item in jsondata:
+            print(item[1])
+            sparkline1.add_value(float(item[1]))
+    except RuntimeError as e:
+        print("Some error occured, retrying! -", e)
+    
     display.auto_refresh = True
-
-    # The display seems to be less jittery if a small sleep time is provided
-    # You can adjust this to see if it has any effect
-    time.sleep(0.01)
+    #gc.collect()
+    print(gc.mem_free())
+    time.sleep(60)
