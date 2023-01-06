ee4586648b8a2ff438b49ce62631bdf710db15d8
axelmagnus <33330896+axelmagnus@users.noreply.github.com>
2023-01-07 00:10:54 +0100

    Milestone. 5 buttons working

--- code.py
+++ code.py
@@ -36,7 +36,7 @@ RESOLUTION = 120  # in minutes 1,2 30, 60, 120
 # Adafruit IO Feeds
 feed_group = 'esp32s2tft'
 # feedname, number of decimals, unit, pretty name
-feed_info = (('temp', 1, "C", 'Temp'), ('hum', 0, "  %", 'Humidity'), ('voltage', 2, "V",'Voltage'), ('percent', 1, "%", 'Batt life'))  
+feed_info = (('temp', 1, "C", 'Temp'), ('hum', 0, "  %", 'Humidity'), ('voltage', 2, "V",'Battery'), ('percent', 1, "%", 'Batt life'))  
 IO_FEEDS = ()
 for feed in feed_info:
     #print(feed[0])
@@ -61,7 +61,7 @@ DATA_LABELS = [
 ]
 DATE_LABEL = Label(text_font, text="000000 00:00",
                    color=DATE_COLOR, x=88, y=130, scale=1)
-STATUS_LABEL = Label(text_font, text="Fetching data...",
+STATUS_LABEL = Label(text_font, text=f"SSID: {secrets['ssid']}",
                      color=0x000000, x=90, y=108)
 GRAPH_LABEL = Label(text_font, text="Temp: 48.0 h",
                     color=0x000000, x=82, y=168)
@@ -87,9 +87,9 @@ button_temp = Button(
     y=display.height-3*TAB_BUTTON_HEIGHT,
     width=TAB_BUTTON_WIDTH,
     height=TAB_BUTTON_HEIGHT,
-    label="Temp",
+    label=feed_info[0][3],
     label_font=text_font,
-    label_color=0x000000,
+    label_color=0xFF00FF,
     fill_color=None,  # 0x5C5B5C,
     outline_color=None,
     selected_fill=0x1A1A1A,
@@ -102,10 +102,10 @@ button_batt = Button(
     x=0,  # Start at furthest left
     y=display.height-2*TAB_BUTTON_HEIGHT,  # Start at top
     width=TAB_BUTTON_WIDTH,  # Calculated width
-    height=TAB_BUTTON_HEIGHT,  # Static height
-    label="Battery",
+    height=TAB_BUTTON_HEIGHT,
+    label=feed_info[2][3],
     label_font=text_font,
-    label_color=0xFFFFFF,
+    label_color=0xFFF499,
     fill_color=None,  # 0x5C5B5C,
     outline_color=None,
     selected_fill=0x1A1A1A,
@@ -119,9 +119,9 @@ button_charge = Button(
     y=display.height-TAB_BUTTON_HEIGHT,  # Start at top
     width=TAB_BUTTON_WIDTH,  # Calculated width
     height=TAB_BUTTON_HEIGHT,  # Static height
-    label="Charge",
+    label=feed_info[3][3],
     label_font=text_font,
-    label_color=0xFFFFFF,
+    label_color=0xFFF499,
     fill_color=None,  # 0x5C5B5C,
     outline_color=None,
     selected_fill=0x1A1A1A,
@@ -139,7 +139,7 @@ button_1w = Button(
     height=TAB_BUTTON_HEIGHT,  # Static height
     label="1 week",
     label_font=text_font,
-    label_color=0x000000,
+    label_color=0xFFF499,
     fill_color=0x00BB33,  # 0x5C5B5C,
     outline_color=0x00BBFF,
     selected_fill=0x1A1A1A,
@@ -154,7 +154,7 @@ button_48h = Button(
     height=TAB_BUTTON_HEIGHT,  # Static height
     label="48 h",
     label_font=text_font,
-    label_color=0x000000,
+    label_color=0xFFF499,
     fill_color=0xBB0033,  # 0x5C5B5C,
     outline_color=0xFF7676,
     selected_fill=0x1A1A1A,
@@ -190,23 +190,24 @@ pyportal.get_local_time()
 lastupdated = datetime.now()
 print("starting")
 now = time.localtime()
+firstrun=True
 
 while True:
     oldnow = now
     now = time.localtime()
     s = "%02d%02d%02d %02d:%02d:%02d" % (
         now.tm_year-2000, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
-    if oldnow.tm_sec != now.tm_sec or not value:#update time to fetch data from
+    if oldnow.tm_sec != now.tm_sec or firstrun:#update time to fetch data from
         DATE_LABEL.text = s  # time now
         ENDTIME = "%04d-%02d-%02d%c%04d:%02d:%02d%c" % (
             now.tm_year, now.tm_mon, now.tm_mday, "T", now.tm_hour, now.tm_min, now.tm_sec, "Z")
     #nu = datetime.now()  # to use for latest publlished data
     try:
         gc.collect()
-        print((datetime.now() - lastupdated).total_seconds()-3600)#-3600 becaause utc
-        if (datetime.now() - lastupdated).total_seconds()-3600 > REFRESH_TIME or not value: 
+        #print((datetime.now() - lastupdated).total_seconds()-3600)#-3600 because utc, update feeds:
+        if (datetime.now() - lastupdated).total_seconds()-3600 > REFRESH_TIME or firstrun: 
             lastupdated = datetime.now()
-            print('Fetching Adafruit IO Feed Value..')
+            STATUS_LABEL.text ='Fetching data...'
             for i, feed in enumerate(IO_FEEDS):
                 liveurl = "https://io.adafruit.com/api/v2/{0}/feeds/{1}/data?X-AIO-Key={2}&limit=1".format(
                     IO_USER, feed, IO_KEY)
@@ -223,21 +224,29 @@ while True:
             date_part, time_part = value[1].split("T")
             year, month, day = date_part.split("-")
             hours, minutes, seconds = time_part[0:-1].split(":")
-            seconds = seconds[0:-1]
+            #seconds = seconds[0:-1]
             adjhours = int(hours) - now.tm_isdst  # TZ or DST or something
             STATUS_LABEL.text = f"Last data: {day}/{month} {adjhours}:{minutes}:{seconds}"
             print("done fetching")
-        touch = ts.touch_point
-        if touch:  # Only do this if the screen is touched
+        
+        if firstrun:
+            touch = (62, 122, 10514) #init with temp
+        else:
+            touch = ts.touch_point
+        
+        if touch:  # Only do this if the screen is touched, or fill buttons with names
             # loop with buttons using enumerate() to number each button group as i
+            print(touch)
             for i, b in enumerate(buttons):
-                b.label_color = 0xFF00FF
-                if b.contains(touch):  # Test each button to see if it was pressed
-                    b.label_color = 0xFF0000
+                if i < 3:
+                    b.label_color = 0xFFF499
+                if b.contains(touch):  # Test each button to see if it was pressed, set data source aand label
+                    sparkfeed_index=0       #init with temp, for firstrun
+                    if i<3:
+                        b.label_color=0x000000  #indicate what feed is pressed/shown
                     if i == 0:
                         print('Temp')
                         sparkfeed_index = 0
-
                         while ts.touch_point:  # for debounce
                             pass
                     if i == 1:
@@ -268,86 +277,86 @@ while True:
                         buttons[3].fill_color = 0xBB0033
                         while ts.touch_point:  # for debounce
                             pass
+            
                     GRAPH_LABEL.text = "Fetching {} {} h".format(
-                        feed_info[sparkfeed_index][3], HOURS)
-                    if 0 <= i <= 4:
-                        DATA_SOURCE = "https://io.adafruit.com/api/v2/{0}/feeds/{1}/data/chart?X-AIO-Key={2}&end_time={3}&hours={4}&resolution={5}".format(
-                            IO_USER, IO_FEEDS[sparkfeed_index], IO_KEY, ENDTIME, HOURS, RESOLUTION)
-                        print(
-                            "_____________________Get sparkline data____________________")
-                        print(DATA_SOURCE)
-                        value = pyportal.network.fetch_data(
-                            DATA_SOURCE, json_path=['data'])
-                        print("Response is", value[0][-1][0])
-                        print("number of rec's", len(value[0]))
-                        print(gc.mem_free())
-                        GRAPH_LABEL.text = f"# of records: {len(value[0])}"
+                                feed_info[sparkfeed_index][3], HOURS)
+                    DATA_SOURCE = "https://io.adafruit.com/api/v2/{0}/feeds/{1}/data/chart?X-AIO-Key={2}&end_time={3}&hours={4}&resolution={5}".format(
+                        IO_USER, IO_FEEDS[sparkfeed_index], IO_KEY, ENDTIME, HOURS, RESOLUTION)
+                    print(
+                        "_____________________Get sparkline data____________________")
+                    print(DATA_SOURCE)
+                    value = pyportal.network.fetch_data(
+                        DATA_SOURCE, json_path=['data'])
+                    print("Response is", value[0][-1][0])
+                    print("number of rec's", len(value[0]))
+                    print(gc.mem_free())
+                    GRAPH_LABEL.text = f"# of records: {len(value[0])}"
 
-                        display.auto_refresh = False
-                        """  
-                        pyportal.splash.remove(sparkline1)
+                    display.auto_refresh = False
+                    """  
+                    pyportal.splash.remove(sparkline1)
+                    gc.collect()
+                    sparkline1 = Sparkline(
+                        width=335, height=85, max_items=len(value[0]), x=80, y=188, color=0x000000)
+                    """
+                    sparkline1.clear_values()  # doesnt clear top and bottom"
+                    #print(sparkline1.y_bottom)
+                    #sparkline1.__init__()
+
+                    """
+                    update()
+                    init?
+                    sparkline1.y_bottom = 100.0
+                    sparkline1.y_top = 0.0
+                    """
+
+                    gc.collect()
+                    #sparkline1.y_top=0
+                    #sparkline1.y_bottom=30
+                    # add the sparkline into my_group
+                    print(gc.mem_free())
+
+                    #print(value[0])
+
+                    max_value = max(value[0], key=lambda x: float(x[1]))
+                    print("max", max_value[1])
+                    min_value = min(value[0], key=lambda x: float(x[1]))
+                    print("min", min_value[1])
+                    """
+                    
+                    sparkline1.y_top = float(max_value[1])
+
+                    sparkline1.y_bottom = float(min_value[1])
+                    """
+                    for item in value[0]:
+                        #print(float(item[1]))
+                        sparkline1.add_value(float(item[1]), update=False)
                         gc.collect()
-                        sparkline1 = Sparkline(
-                            width=335, height=85, max_items=len(value[0]), x=80, y=188, color=0x000000)
-                        """
-                        sparkline1.clear_values()  # doesnt clear top and bottom"
-                        #print(sparkline1.y_bottom)
-                        #sparkline1.__init__()
+                    #display.show(pyportal.splash)
+                    print(sparkline1.values())
+                    print("min spartlines", min(sparkline1.values()))
+                    GRAPH_LO_LABEL.text = f"%.*f %s" % (
+                        feed_info[sparkfeed_index][1], min(sparkline1.values()), feed_info[sparkfeed_index][2])
+                    print("max sparklines", max(sparkline1.values()))
+                    GRAPH_HI_LABEL.text = f"%.*f %s" % (
+                        feed_info[sparkfeed_index][1], max(sparkline1.values()), feed_info[sparkfeed_index][2])
+                    print("bottom top")
+                    print(sparkline1.y_bottom)
+                    #sparkline1.y_top = max(sparkline1.values())
 
-                        """
-                        update()
-                        init?
-                        sparkline1.y_bottom = 100.0
-                        sparkline1.y_top = 0.0
-                        """
-
-                        gc.collect()
-                        #sparkline1.y_top=0
-                        #sparkline1.y_bottom=30
-                        # add the sparkline into my_group
-                        print(gc.mem_free())
-
-                        #print(value[0])
-
-                        max_value = max(value[0], key=lambda x: float(x[1]))
-                        print("max", max_value[1])
-                        min_value = min(value[0], key=lambda x: float(x[1]))
-                        print("min", min_value[1])
-                        """
-                        
-                        sparkline1.y_top = float(max_value[1])
-
-                        sparkline1.y_bottom = float(min_value[1])
-                        """
-                        for item in value[0]:
-                            #print(float(item[1]))
-                            sparkline1.add_value(float(item[1]), update=False)
-                            gc.collect()
-                        #display.show(pyportal.splash)
-                        print(sparkline1.values())
-                        print("min spartlines", min(sparkline1.values()))
-                        GRAPH_LO_LABEL.text = f"%.*f %s" % (
-                            feed_info[sparkfeed_index][1], min(sparkline1.values()), feed_info[sparkfeed_index][2])
-                        print("max sparklines", max(sparkline1.values()))
-                        GRAPH_HI_LABEL.text = f"%.*f %s" % (
-                            feed_info[sparkfeed_index][1], max(sparkline1.values()), feed_info[sparkfeed_index][2])
-                        print("bottom top")
-                        print(sparkline1.y_bottom)
-                        #sparkline1.y_top = max(sparkline1.values())
-
-                        #sparkline1.y_bottom = min(sparkline1.values())
-                        print(sparkline1.y_top)
-                        #sparkline1.y_bottom = min(sparkline1.values())
-                        """
-                        sparkline1.y_max=15
-                        sparkline1.min_value=3
-                        sparkline1.max_value=20
-                        """
-                        sparkline1.update()
-                        display.auto_refresh = True
-                        GRAPH_LABEL.text = "{}: {} h R:{} h #:{:d}".format(
-                            feed_info[sparkfeed_index][3], len(value[0])*RESOLUTION/60, RESOLUTION/60, len(value[0]))
-
+                    #sparkline1.y_bottom = min(sparkline1.values())
+                    print(sparkline1.y_top)
+                    #sparkline1.y_bottom = min(sparkline1.values())
+                    """
+                    sparkline1.y_max=15
+                    sparkline1.min_value=3
+                    sparkline1.max_value=20
+                    """
+                    sparkline1.update()
+                    display.auto_refresh = True
+                    GRAPH_LABEL.text = "{}: {} h R:{} h #:{:d}".format(
+                        feed_info[sparkfeed_index][3], len(value[0])*RESOLUTION/60, RESOLUTION/60, len(value[0]))
+        firstrun=False
     except MemoryError as e:
         traceback.print_exception(type(e), e, e.__traceback__)
         print("Some error occured, retrying! -", e)
